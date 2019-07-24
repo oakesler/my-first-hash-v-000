5fd827e3f4e4d3ba4019400099095313bdb75b10
Olivia Kesler <oakesler@protonmail.ch>
2019-07-24 05:41:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {chef => "Duncan", pastry}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {chef => "Duncan", pastry chef => "Chelsie", cook => "Will"}
 
   
 end
