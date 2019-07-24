33df0abbbf42e8d5bae8e1670506cd22aa30a6d8
Olivia Kesler <oakesler@protonmail.ch>
2019-07-24 05:40:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {ch}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {chef =>}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
